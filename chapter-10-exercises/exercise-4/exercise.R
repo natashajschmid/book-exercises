ce8bf37884624f5d77e25cdee4ca7b675ff76acf
Natasha Schmid <njschmid@uw.edu>
2020-01-28 14:43:13 -0800

    Add week 4 exercises

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -5,35 +5,36 @@
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
 
+gates_money <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(gates_money)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- gates_money[ , "organization"]
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean(gates_money[ , "total_amount"])
 
 # What was the dollar amount of the largest grant?
-
+max(gates_money[ , "total_amount"])
 
 # What was the dollar amount of the smallest grant?
-
+min(gates_money[ , "total_amount"])
 
 # Which organization received the largest grant?
-
+gates_money[gates_money[ , "total_amount"] == max(gates_money[ , "total_amount"]), "organization"]
 
 # Which organization received the smallest grant?
-
+gates_money[gates_money[ , "total_amount"] == min(gates_money[ , "total_amount"]), "organization"]
 
 # How many grants were awarded in 2010?
-
+nrow(gates_money)
